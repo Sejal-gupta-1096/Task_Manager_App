bba35e51ea50a9ea59dca197782ee43c54f85131
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-05-17 12:13:35 +0530

    User sign up page created along with storing user in database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,17 @@
+const Users = require("../models/users");
+
 module.exports.signUp = function(request , response){
     return response.render("sign-up");
+}
+
+module.exports.createUser = function(request , response){
+   Users.findOne({email:request.body.email} , function(error , user){
+    if(user){
+        return response.redirect("back");
+    }else{
+        Users.create(request.body); 
+        return response.redirect("back");
+    }
+   }); 
+
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const usersController = require("../controllers/users_controller");
 
 router.get("/sign-up" , usersController.signUp);
+router.post("/create-user" , usersController.createUser);
 
 module.exports = router;
